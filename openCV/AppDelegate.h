6003c1ffcef8f46170aab240b0913bfd7cd524c2
iuimini5 <TobyHsu@iuimini5tekiMac-mini.local>
2015-03-25 14:47:13 +0800

    Initial Commit

---
+++ openCV/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  openCV
+//
+//  Created by iuimini5 on 2015/3/25.
+//  Copyright (c) 2015年 iuimini5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ openCV/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  openCV
+//
+//  Created by iuimini5 on 2015/3/25.
+//  Copyright (c) 2015年 iuimini5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
